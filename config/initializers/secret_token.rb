8a33d7728c65f0871c195dc10808678c7ae54509
Matthew Fisher <matthewf@activestate.com>
2013-08-30 22:58:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloworldRails::Application.config.secret_token = '3d43bcdf5f9397db43efb323ce2a11d1d0aab3001f02958bd6b211871082f91fe023040a8ed97e34532d3d05cfec0990ac6f5ff235ea8e5ec17c87e4e3c9acf9'
